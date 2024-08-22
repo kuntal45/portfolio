46eca20d10815691b2849e03a0cf013109643f0f
kuntal Maity <143471213+kuntal45@users.noreply.github.com>
2024-08-22 11:11:36 +0000

    chore: Add Next.js configuration files and dependencies

---
+++ src/app/layout.tsx
@@ -0,0 +1,29 @@
+import type { Metadata } from "next";
+import { IBM_Plex_Sans } from "next/font/google";
+import "./globals.css";
+import { cn } from "@/lib/utils";
+import Navbar from "@/components/shared/Navbar";
+
+const IBMPlex = IBM_Plex_Sans({ 
+  subsets: ["latin"],
+  weight: ['400', '500', '600', '700'],
+  variable: '--font-ibm-plex'
+});
+export const metadata: Metadata = {
+  title: "Portfolio",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" className="dark">
+    <body className={cn("font-IBMPlex antialiased", IBMPlex.variable)}>
+      <Navbar />
+      {children}
+    </body>
+  </html>
+  );
+}
